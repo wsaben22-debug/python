2119b750f915dc97ffaaac0d4b8dbec050f29078
wsaben22-debug <wsaben22@gmail.com>
2026-07-21 14:10:22 +0800

    變數_int

---
+++ first project/CH02/變數.py
@@ -0,0 +1,9 @@
+yesNo ='y'  
+pssScore, maxScore, minScore = 0, 0, 100
+power1 = power2 = 100
+total = 1.23456E+6 #123456.0
+print(yesNo)
+print(pssScore, maxScore, minScore)
+print(power1, power2)
+del total #del 刪除的意思
+print(total) #報錯, 已經刪除
